0580a0efb1f02841f89db734f6ff1e23c16ef8f5
Keith Zantow <keith.zantow@owensoftware.com>
2013-09-05 14:49:25 -0400

    Add OSC Voting Script

--- osc-vote-with-sc-player.js
+++ osc-vote-with-sc-player.js
@@ -36,8 +36,8 @@ var jQueryLoaded = function() {
 					$this.append(
 					'<iframe width="100%" height="166" scrolling="no" frameborder="no" '
 					+'src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F'+track.id+'"></iframe>'
-					+'<br/><br/>'
-					);
+					)
+					.parents('.ss-form-entry:first').append('<br/><br/>')
 				}
 				
 				var appendTrackIfMatch = function(track) {
